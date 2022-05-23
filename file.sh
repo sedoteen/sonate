09d32ac9b9842437177bbb22fab9f2f931c9158a
sedoten <ola.ebenezer55@gmail.com>
2022-05-23 13:01:07 -0400

    first version

---
+++ file.sh
@@ -0,0 +1,4 @@
+tjgfhbvkhgfkhbvnjb
+utkjyhyftdgvhbuiytdfrxcvb
+dtgfhvn
+dytgcbvytfxvcbvnkgfjghcvbnhvg
---
+++ random.sh
@@ -0,0 +1,9 @@
+fkfjhv atygchvmn .jhgflyuvkyzj
+fxghc,nbihrserxv lhb
+tdtc,jhvlki9ou;oiuy
+tyiykfhvhbvnb nbhjvcxdzdsrdky
+gdtrjwjsthcgb,n.lk;
+drgcbnv, nmjkuytyc
+fdfdtgfghfvhhhhvjhbjhgdfxfvui
+jt6r5rerdfghjkjhgfcvbn
+jytrdfgbnfdc
